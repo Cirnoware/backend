e6044884a709c69b66d26b241898693dffa02bfa
Cirno <3220103583@zju.edu.com>
2025-02-17 16:54:06 +0800

    实现单向部分元件的新建与数据更改

--- blueprints/Homepage.py
+++ blueprints/Homepage.py
@@ -3,40 +3,86 @@ from models import Homepage
 
 homepage = Blueprint('homepage', __name__, url_prefix="/Homepage")
 
-# 格式如下: 127.0.0.1/Homepage/getData?name=xxx
-@homepage.route('/getData', strict_slashes=False, methods=['GET', 'POST'])
-def get_data():
+# 格式如下: 127.0.0.1/Homepage/getFile?name=xxx
+@homepage.route('/getFile', strict_slashes=False, methods=['GET', 'POST'])
+def get_file():
     id = request.args.get('id')
     name = request.args.get('name')
-    # print(f"trying to get data of '{name}'")  # 这个print是用来调试的,会在运行app.py的终端中显示
-    response = Homepage.get_data(id)
+    # print(f"trying to get File of '{name}'")  # 这个print是用来调试的,会在运行app.py的终端中显示
+    response = Homepage.get_file(id)
     # print("response is:",response)
     return response
 
 # 谨慎使用，这个真的能删除数据
-@homepage.route('/deleteData', strict_slashes=False, methods=['GET', 'POST'])
-def delete_data():
+@homepage.route('/deleteFile', strict_slashes=False, methods=['GET', 'POST'])
+def delete_file():
     id = request.args.get('id')
     name = request.args.get('name')
-    response = Homepage.delete_data(id)
+    response = Homepage.delete_file(id)
     return response
 
-@homepage.route('/createData', strict_slashes=False, methods=['GET', 'POST'])
-def create_data():
+@homepage.route('/createFile', strict_slashes=False, methods=['GET', 'POST'])
+def create_file():
     # name = request.args.get('name')
-    response = Homepage.create_data()
+    response = Homepage.create_file()
     return response
 
 # 重命名
-@homepage.route('/renameData', strict_slashes=False, methods=['GET', 'POST'])
-def rename_data():
+@homepage.route('/renameFile', strict_slashes=False, methods=['GET', 'POST'])
+def rename_file():
     id = request.args.get('id')
     newname = request.args.get('newname')
-    response = Homepage.rename_data(id,newname)
+    response = Homepage.rename_file(id,newname)
     return response
 
-# 获取所有数据的条目，因为数据量还不是很大，所以把data的内容也一并传回
-@homepage.route('/getDataList', strict_slashes=False, methods=['GET', 'POST'])
-def get_data_list():
-    response = Homepage.get_data_list()
+# 获取所有数据的条目，因为数据量还不是很大，所以把File的内容也一并传回
+@homepage.route('/getFileList', strict_slashes=False, methods=['GET', 'POST'])
+def get_file_list():
+    response = Homepage.get_file_list()
+    return response
+
+@homepage.route('/addComponent', strict_slashes=False, methods=['GET', 'POST'])
+def add_component():
+    f_id = request.args.get('f_id')  # f即为file
+    c_id = request.args.get('c_id')  # c即为component
+    c_name = request.args.get('c_name')
+    c_type = request.args.get('c_type')
+    response = Homepage.add_component(f_id,c_id,c_name,c_type)
+    return response
+
+@homepage.route('/changeBatteryStorageParam', strict_slashes=False, methods=['GET', 'POST'])
+def change_battery_storage_param():
+    f_id = request.args.get('f_id')
+    c_id = request.args.get('c_id')
+    rated_voltage = request.args.get('rated_voltage')
+    capacity = request.args.get('capacity')
+    resistance = request.args.get('resistance')
+    response = Homepage.change_battery_storage_param(f_id,c_id,rated_voltage,capacity,resistance)
+    return response
+
+@homepage.route('/changeWindParam', strict_slashes=False, methods=['GET', 'POST'])
+def change_wind_param():
+    f_id = request.args.get('f_id')
+    c_id = request.args.get('c_id')
+    rated_power = request.args.get('rated_power')
+    cutin_speed = request.args.get('cutin_speed')
+    cutout_speed = request.args.get('cutout_speed')
+    response = Homepage.change_wind_param(f_id, c_id, rated_power, cutin_speed, cutout_speed)
+    return response
+
+@homepage.route('/changeSolarParam', strict_slashes=False, methods=['GET', 'POST'])
+def change_solar_param():
+    f_id = request.args.get('f_id')
+    c_id = request.args.get('c_id')
+    peak_power = request.args.get('peak_power')
+    efficiency = request.args.get('efficiency')
+    open_voltage = request.args.get('open_voltage')
+    response = Homepage.change_solar_param(f_id, c_id, peak_power, efficiency, open_voltage)
+    return response
+
+@homepage.route('/deleteComponent', strict_slashes=False, methods=['GET', 'POST'])
+def delete_component():
+    f_id = request.args.get('f_id')
+    c_id = request.args.get('c_id')
+    response = Homepage.delete_component(f_id, c_id)
     return response
--- models/Homepage.py
+++ models/Homepage.py
@@ -1,38 +1,168 @@
-from models.utils import DB
+from models.utils import DB,BatteryStorage
 from flask import jsonify
 from datetime import datetime
+import os,json
 
-def get_data(id):
+
+
+
+def get_file(id):
     # 这部分的代码参考服务器端的代码，在EEEIC/Meter/backend/中
     # 这个代码可能会有SQL注入的问题，请不要尝试攻击
     sql = f"SELECT data FROM home WHERE id = '{id}';"    # 使用f-string格式化字符串
     response = DB.query(sql)
     return jsonify(response)    # 必须要json一下
 
-def delete_data(id):
+def delete_file(id):
     sql = f"DELETE FROM home WHERE id = '{id}';"
     response = DB.delete(sql)
     return jsonify(response)    # 必须要json一下
 
-def create_data():  # 只生成id，不设置名称
+def create_file():  # 只生成id，不设置名称
     # 获取当前时间
     current_time = datetime.now()
     # 格式化时间
     formatted_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
     id = current_time.strftime('%Y%m%d%H%M%S')  # 为避免重复，id为创建数据时的时间
     print(id)
-    data = 'this is ' + str(id)
+
+    # 生成默认的仿真文件内容
+    base_dir = os.path.dirname(__file__)  # 获取 B.py 所在目录
+    file_path = os.path.join(base_dir, "..\\src", "defaultFile.json")  # 拼接出 D.json 的路径
+    with open(file_path, 'r') as f:
+        DEFAULT_FILE = f.read()
+    data = DEFAULT_FILE
+
+    # 将结果写入数据库
     sql = f"INSERT INTO `eeeic`.`home` (`id`, `data`, `create_time`) VALUES ('{id}', '{data}', '{formatted_time}')"
     DB.create(sql)
     return jsonify({"id": id})
 
-def rename_data(id,newname):
+def rename_file(id,newname):
     sql = f"UPDATE eeeic.home SET name = '{newname}' WHERE id = '{id}'"
     response = DB.update(sql)
     return jsonify(response)
 
-def get_data_list():
+def get_file_list():
     sql = "SELECT id, name FROM eeeic.home;"
     response = DB.query(sql)
     formatted_response = [{'id': row[0], 'name': row[1]} for row in response] 
     return jsonify(formatted_response)
+
+def add_component(f_id,c_id,c_name,c_type):
+    # print(f_id,c_id,c_name,c_type)
+    sql = f"SELECT data FROM eeeic.home WHERE id = '{f_id}';"
+    f_data = DB.query(sql)
+    f_data = json.loads(f_data[0][0])
+
+    new_data = ""
+    if (c_type == 'BatteryStorage'):
+        c_data = {
+            "c_id": f"{c_id}",
+            "c_type": f"{c_type}",
+            "c_name": f"{c_name}",
+            "parameters": {
+                "capacity": 1000,
+                "rated_voltage": 500,
+                "resistance": 0.95
+            },
+            "connection": {
+                "type": "AC",
+                "interface": "grid"
+            }
+        }
+    elif (c_type == 'Wind'):
+        c_data = {
+            "c_id": f"{c_id}",
+            "c_type": f"{c_type}",
+            "c_name": f"{c_name}",
+            "parameters": {
+                "rated_power": 1000,
+                "cutin_speed": 3,
+                "cutout_speed": 25
+            },
+            "connection": {
+                "type": "AC",
+                "interface": "grid"
+            }
+        }
+    elif (c_type == 'Solar'):
+        c_data = {
+            "c_id": f"{c_id}",
+            "c_type": f"{c_type}",
+            "c_name": f"{c_name}",
+            "parameters": {
+                "rated_power": 1000,
+                "efficiency": 0.2,
+                "open_voltage": 500
+            },
+            "connection": {
+                "type": "AC",
+                "interface": "grid"
+            }
+        }
+    f_data["devices"].append(c_data)
+    new_data = json.dumps(f_data, indent=4, ensure_ascii=False)
+    # print(new_data)
+    sql = f"UPDATE eeeic.home SET data = '{new_data}' WHERE id = '{f_id}';"
+    response = DB.update(sql)
+    # print(response)
+    return jsonify(response)
+
+def change_battery_storage_param(f_id,c_id,rated_voltage,capacity,resistance):
+    sql = f"SELECT data FROM eeeic.home WHERE id = '{f_id}';"
+    f_data = DB.query(sql)
+    f_data = json.loads(f_data[0][0])
+    for device in f_data["devices"]:
+        if device["c_id"] == c_id:
+            device["parameters"]["capacity"] = capacity
+            device["parameters"]["rated_voltage"] = rated_voltage
+            device["parameters"]["resistance"] = resistance
+            break
+    new_data = json.dumps(f_data, indent=4, ensure_ascii=False)
+    sql = f"UPDATE eeeic.home SET data = '{new_data}' WHERE id = '{f_id}';"
+    response = DB.update(sql)
+    return jsonify(response)
+
+def change_wind_param(f_id,c_id,rated_power,cutin_speed,cutout_speed):
+    sql = f"SELECT data FROM eeeic.home WHERE id = '{f_id}';"
+    f_data = DB.query(sql)
+    f_data = json.loads(f_data[0][0])
+    for device in f_data["devices"]:
+        if device["c_id"] == c_id:
+            device["parameters"]["rated_power"] = rated_power
+            device["parameters"]["cutin_speed"] = cutin_speed
+            device["parameters"]["cutout_speed"] = cutout_speed
+            break
+    new_data = json.dumps(f_data, indent=4, ensure_ascii=False)
+    sql = f"UPDATE eeeic.home SET data = '{new_data}' WHERE id = '{f_id}';"
+    response = DB.update(sql)
+    return jsonify(response)
+
+def change_solar_param(f_id,c_id,rated_power,efficiency,open_voltage):
+    sql = f"SELECT data FROM eeeic.home WHERE id = '{f_id}';"
+    f_data = DB.query(sql)
+    f_data = json.loads(f_data[0][0])
+    for device in f_data["devices"]:
+        if device["c_id"] == c_id:
+            device["parameters"]["rated_power"] = rated_power
+            device["parameters"]["efficiency"] = efficiency
+            device["parameters"]["open_voltage"] = open_voltage
+            break
+    new_data = json.dumps(f_data, indent=4, ensure_ascii=False)
+    sql = f"UPDATE eeeic.home SET data = '{new_data}' WHERE id = '{f_id}';"
+    response = DB.update(sql)
+    return jsonify(response)
+
+def delete_component(f_id,c_id):
+    sql = f"SELECT data FROM eeeic.home WHERE id = '{f_id}';"
+    f_data = DB.query(sql)
+    f_data = json.loads(f_data[0][0])
+    for device in f_data["devices"]:
+        if device["c_id"] == c_id:
+            f_data["devices"].remove(device)
+            break
+    new_data = json.dumps(f_data, indent=4, ensure_ascii=False)
+    sql = f"UPDATE eeeic.home SET data = '{new_data}' WHERE id = '{f_id}';"
+    response = DB.update(sql)
+    return jsonify(response)
